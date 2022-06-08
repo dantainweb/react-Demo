f9abb881bb48a9e2ffd1ba0986782790f2f01a77
dantain <d5361920@gmail.com>
2022-06-08 22:46:19 +0800

    成功

--- src/component/loginPage.js
+++ src/component/loginPage.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 
-export default class LoginPage {
+export default class LoginPage extends PureComponent {
 
     render() {
         return (
-            `<form>
+            <form>
                 帳號：<input type="text" /><br />
                 密碼：<input type="password" />
             </form>
-            `
+
 
         )
     }
