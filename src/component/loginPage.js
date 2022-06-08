36aa24c819c8dff0d2761082f5cbda296e8ba8f2
dantain <d5361920@gmail.com>
2022-06-08 21:58:15 +0800

    測試推一小版

---
+++ src/component/loginPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class LoginPage {
+
+    render() {
+        return (
+            <form>
+                帳號：<input type="text" /><br />
+                密碼：<input type="password" />
+            </form>
+
+        )
+    }
+}
