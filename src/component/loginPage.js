54b0f3c898a913a92598bc3b5065e40a84c831f4
dantain <d5361920@gmail.com>
2022-06-08 22:40:46 +0800

    正確的loginPage

---
+++ src/component/loginPage.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export default class LoginPage extends PureComponent {
+
+
+    render() {
+        return (
+            <form>
+                <input type="text" />
+            </form>
+        )
+    }
+}
